85ea01b8c2c9098a3b2c2525974487de8699d220
Tawanda Makunike <tawanda@mmogomedia.com>
2022-10-20 20:34:21 -0700

    Updates

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return:  Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+		return (c >= 'A' && c <= 'Z');
+}
